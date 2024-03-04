d9277786e4e1fc5f0bb8d7fc34da6d6f69cfe75e
Vincenzolaveglia <vincenzolaveglia10@gmail.com>
2024-03-04 16:49:17 +0100

    Aggiungere i file di progetto.

---
+++ Models/Login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gestionale_Spedizioni.Models
+{
+    public class Login
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Email { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
---
+++ Models/Spedizione.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gestionale_Spedizioni.Models
+{
+    public class Spedizione
+    {
+        public int Id { get; set; }
+        public int ClienteId { get; set; }
+        public string Mittente { get; set; }
+        public string CodiceSpedizione { get; set; }
+        public string NomeDestinatario { get; set; }
+        public string IndirizzoDestinazione { get; set; }
+        public string CittaDestinazione { get; set; }
+        public decimal Costo { get; set; }
+        public decimal PesoKg { get; set; }
+        public string DataSpedizione { get; set; }
+        public string DataStimataConsegna { get; set; }
+    }
+    public class Stato
+    {
+        public int Id { get; set; }
+        public int SpedizioneId { get; set; }
+        public string Aggiornamento { get; set; }
+        public string Luogo { get; set; }
+        public string Descrizione { get; set; }
+        public DateTime DataOraAggiornamento { get; set; }
+    }
+
+    public class DettaglioSpedizione
+    {
+        public Spedizione Spedizione { get; set; }
+        public List<Stato> Stato { get; set; }
+    }
+}
